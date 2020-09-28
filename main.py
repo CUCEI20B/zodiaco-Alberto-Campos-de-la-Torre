7d4c0ef5771f9b100ede980758dd767ec5f838f7
Alberto Campos de la Torre <alberto.campos3077@alumnos.udg.mx>
2020-09-28 15:53:43 -0500

    Prueba05

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ mes = 6
 
 if  (31>= dia >=21 and mes ==3)or(1<= dia<=20 and mes==4):
        print("aries")
-elif  (30>= dia >=21 and mes ==4)or(1<= dia<=20 and mes==5):
+if (30>= dia >=21 and mes ==4)or(1<= dia<=20 and mes==5):
        print("tauro")
-elif  (31>= dia >=21 and mes ==5)or(1<= dia<=21 and mes==6):
+if  (31>= dia >=21 and mes ==5)or(1<= dia<=21 and mes==6):
        print("geminis")
-elif  (30>= dia >=22 and mes ==6)or(1<= dia<=22 and mes==7):
+if  (30>= dia >=22 and mes ==6)or(1<= dia<=22 and mes==7):
        print("cancer")
-elif  (31>= dia >=23 and mes ==7)or(1<= dia<=22 and mes==8):
+if (31>= dia >=23 and mes ==7)or(1<= dia<=22 and mes==8):
        print("leo")
-elif  (31>= dia >=23 and mes ==8)or(1<= dia<=22 and mes==9):
+if  (31>= dia >=23 and mes ==8)or(1<= dia<=22 and mes==9):
        print("virgo")
-elif  (30>= dia >=23 and mes ==9)or(1<= dia<=22 and mes==10):
+if (30>= dia >=23 and mes ==9)or(1<= dia<=22 and mes==10):
        print("libra")
-elif  (31>= dia >=23 and mes ==10)or(1<= dia<=22 and mes==11):
+if  (31>= dia >=23 and mes ==10)or(1<= dia<=22 and mes==11):
        print("escorpio")
-elif  (30>= dia >=23 and mes ==11)or(1<= dia<=21 and mes==12):
+if  (30>= dia >=23 and mes ==11)or(1<= dia<=21 and mes==12):
        print("sagitario")
-elif  (31>= dia >=22 and mes ==12)or(1<= dia<=20 and mes==1):
+if  (31>= dia >=22 and mes ==12)or(1<= dia<=20 and mes==1):
        print("capricornio")
-elif  (31>= dia >=21 and mes ==1)or(1<= dia<=18 and mes==2):
+if  (31>= dia >=21 and mes ==1)or(1<= dia<=18 and mes==2):
        print("aquario")
-elif  (29>= dia >=19 and mes ==2)or(1<= dia<=20 and mes==3):
+if  (29>= dia >=19 and mes ==2)or(1<= dia<=20 and mes==3):
        print("pisis")
